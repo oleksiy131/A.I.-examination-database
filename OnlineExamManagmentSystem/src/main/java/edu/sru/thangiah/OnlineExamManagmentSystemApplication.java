8badad270ab53ab8a6e689d83e2b8d29bdfa2d85
Seth Chritzman <'>
2023-09-02 09:12:01 -0400

    Initial Commit

---
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/OnlineExamManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.sru.thangiah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamManagmentSystemApplication.class, args);
+	}
+
+}
