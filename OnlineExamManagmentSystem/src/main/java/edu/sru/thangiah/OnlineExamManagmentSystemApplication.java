7b6784f63bfd1095f850f4ec2a68bada9d8e2b02
oleksii <oxd1005@sru.edu>
2023-09-05 14:26:37 -0400

    MainFile Comments

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/OnlineExamManagmentSystemApplication.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/OnlineExamManagmentSystemApplication.java
@@ -1,9 +1,26 @@
-/* */
-/* */ 
-/* */
-/* */
-/* */
-/* */
+/**
+ * ----------------------------------------------------------------------------
+ * File: OnlineExamManagementSystemApplication.java
+ * Date: 9/5/2023
+ * Class:CPSC 488
+ * Professor: Dr. Thangiah
+ * Authors: Seth Chritzman, Brent Kosior, Oleksii Dukhovenko
+ * ----------------------------------------------------------------------------
+ * Description:
+ *
+ * Online Exam-taking software with different user permissions. 
+ *
+ * ----------------------------------------------------------------------------
+ * Revision History:
+ *
+ * Date             Author          Description
+ * ----------------------------------------------------------------------------
+ * [9/5/2023]       [Oleksii]     	Login Page 
+ * [9/5/2023]       [Seth]      	Domain files   
+ * [9/4/2023]       [Brent]    		Templates 
+ * [9/5/2023]       [Seth + Brent]  NAV-Bar 
+ * ----------------------------------------------------------------------------
+ */
 
 package edu.sru.thangiah;
 
