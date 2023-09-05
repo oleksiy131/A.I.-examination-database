0f17deee0279297154e5e5ea3d3e24a9bea3286d
oleksii <oxd1005@sru.edu>
2023-09-05 11:18:40 -0400

    Admin deleted

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/oems/repository/AdministratorRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sru.thangiah.oems.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import edu.sru.thangiah.oems.domain.Administrator;
-
-
-
-public interface AdministratorRepository extends CrudRepository<Administrator, Long> {}
