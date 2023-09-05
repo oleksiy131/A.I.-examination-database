fbea0e7d59c648fb2b4be792b62ceb3c816f5b79
Seth Chritzman <'>
2023-09-04 23:33:04 -0400

    Incorperated class for administrator, constructors were made

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/oems/domain/Administrator.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/oems/domain/Administrator.java
@@ -1,5 +1,54 @@
+/* Seth Chritzman 																				   */
+/* Java Persistence is the API for the management for persistence and object/relational mapping.   */
+/* https://docs.oracle.com/javaee/7/api/javax/persistence/package-summary.html#package.description */
+/* */
+/* */
 package edu.sru.thangiah.oems.domain;
 
-public class Administrator {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.springframework.lang.NonNull;
 
+@Entity
+public class Administrator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long AdministratorID;
+    @NonNull
+    private String AdministratorUsername;
+    @NonNull
+    private String AdministratorPassword;
+    
+    // Getter for Administrator ID
+    public Long getAdministratorID() {
+		return AdministratorID;
+			
+	}
+    
+    // Setter for Administrator ID
+	public void setAdministratorID(Long AdministratorID) {
+		this.AdministratorID = AdministratorID;
+	}
+	
+    // Getter for Administrator Username
+	public String getAdministratorUsername() {
+		return AdministratorUsername;
+	}
+	
+    // Setter for Administrator Username
+	public void setAdministratorUsernamee(String AdministratorUsername) {
+		this.AdministratorUsername = AdministratorUsername;
+	}
+    // Getter for Administrator Password
+	public String getAdministratorPassword() {
+		return AdministratorPassword;
+	}
+    // Setter for Administrator Password
+	public void setAdministratorPassword(String AdministratorPassword) {
+		this.AdministratorPassword = AdministratorPassword;
+	}
+
+    // Constructors, getters, and setters, will eventually add more 
 }
