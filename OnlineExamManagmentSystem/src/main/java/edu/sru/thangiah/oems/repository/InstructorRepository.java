19b89c41c155002d23043aa852b60bd1420150a1
Seth Chritzman <'>
2023-09-06 16:06:34 -0400

    Removed oems package, it was unneeded.

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/oems/repository/InstructorRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sru.thangiah.oems.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import edu.sru.thangiah.oems.domain.Instructor;
-
-
-
-public interface InstructorRepository extends CrudRepository<Instructor, Long> {}
