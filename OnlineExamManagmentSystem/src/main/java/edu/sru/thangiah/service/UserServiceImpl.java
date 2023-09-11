29af78f6a28b53694367cb39541ea32df7ba07bb
Seth Chritzman <'>
2023-09-10 22:30:35 -0400

    Created a user service package to handle data transfer and its implementation

---
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package edu.sru.thangiah.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.sru.thangiah.model.Roles;
+import edu.sru.thangiah.model.User;
+import edu.sru.thangiah.repository.UserRepository;
+import edu.sru.thangiah.web.dto.UserRegistrationDto;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public UserServiceImpl(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public User save(UserRegistrationDto registrationDto) {
+		User user = new User(registrationDto.getFirstName(), registrationDto.getLastName(), registrationDto.getEmail(), registrationDto.getPassword(), 
+			    registrationDto.getUsername(), "ROLE_USER");
+		
+		return userRepository.save(user);
+	}
+
+}
