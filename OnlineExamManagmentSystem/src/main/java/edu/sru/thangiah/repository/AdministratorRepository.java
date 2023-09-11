60c20db1d9c7e1c81900fb01e25ee1a0eff0cee7
oleksii <oxd1005@sru.edu>
2023-09-11 19:45:11 -0400

    DeletedRoleSelection

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/repository/AdministratorRepository.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/repository/AdministratorRepository.java
@@ -8,10 +8,11 @@
 
 package edu.sru.thangiah.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
 import edu.sru.thangiah.domain.Administrator;
 
-public interface AdministratorRepository extends JpaRepository<Administrator, Long> {
+public interface AdministratorRepository extends CrudRepository<Administrator, Long> {
     // Add custom query methods here:
 }
 
