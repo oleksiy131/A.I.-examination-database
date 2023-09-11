889f436f9ece0b261234bd06a6d995b8a13a88c9
oleksii <oxd1005@sru.edu>
2023-09-11 15:24:13 -0400

    11

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/OnlineExamManagmentSystemApplication.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/OnlineExamManagmentSystemApplication.java
@@ -33,6 +33,8 @@ public class OnlineExamManagmentSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineExamManagmentSystemApplication.class, args);
 		
+		
+		
 	}
 }
 
