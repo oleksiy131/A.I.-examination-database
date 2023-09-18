f59bef876e84ff562f8352e90390ec56690551e8
brent <bmk1019@sru.edu>
2023-09-18 14:08:26 -0400

    small updates to EmailVerificationService
    still does not work

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/service/EmailVerificationService.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/service/EmailVerificationService.java
@@ -1,21 +1,20 @@
 package edu.sru.thangiah.service;
 
-import org.springframework.stereotype.Service;
-import edu.sru.thangiah.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-//import org.springframework.boot.context.event.ApplicationReadyEvent;
-//import org.springframework.context.event.EventListener;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import edu.sru.thangiah.model.User;
+
+import java.util.Date;
 
 @Service
 public class EmailVerificationService {
-		@Autowired
-		private JavaMailSender VerificationSender;
-		
+	
 		//@EventListener(ApplicationReadyEvent.class)
-		public void sendMail(User user){
+		public void sendMail (User user){
     	//
     	
     	String name = user.getUsername();
@@ -26,21 +25,32 @@ public class EmailVerificationService {
 		
         System.out.println(name+"  "+email);
         
-        String EmailContent = name+"this is a test and I really want it to work";
+        String EmailContent = name+" this is a test and I really want it to work";
         
         //ToEmail: SubjectLine: Body:
     	sendVerifyEmail(email,"Verification",EmailContent);
 	}
+		
+	@Autowired
+	private JavaMailSender VerificationSender;
 	
 	public void sendVerifyEmail(String toEmail, String subject,String body) {
 	
+
 		SimpleMailMessage message= new SimpleMailMessage();
 		message.setFrom("oemsexamsystem@gmail.com");
 		message.setTo(toEmail);
+		message.setSentDate(new Date());
+		message.setReplyTo("");
 		message.setText(body);
 		message.setSubject(subject);
 		
+		System.out.println(message);
+		try {
 		VerificationSender.send(message);
+		}
+		catch(MailException e) {
+		    e.printStackTrace(); // This will print the exception stack trace to the console
+		}
 	}
-
 }
