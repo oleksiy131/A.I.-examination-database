bae639520b2974e5204ff2575721a55e767b57a5
Seth Chritzman <'>
2023-09-19 13:13:11 -0400

    Reconfigured

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/model/Roles.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/model/Roles.java
@@ -1,5 +1,5 @@
-package edu.sru.thangiah.model;
 
+package edu.sru.thangiah.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,29 +8,35 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
+
 @Table(name = "roles")
 public class Roles {
+
 	@Id
+
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	public Roles(String name) {
 		super();
 		this.name = name;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 }
