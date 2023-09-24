48a1e44ef9fe693bfe4114ab4b3ba139b582b22a
Seth Daniel Chritzman <142919196+sdc1004@users.noreply.github.com>
2023-09-24 15:43:30 -0400

    Removed unneeded tag.

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/repository/UserRepository.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/repository/UserRepository.java
@@ -1,14 +1,11 @@
 package edu.sru.thangiah.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository; 
 import edu.sru.thangiah.model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Add custom query methods here:
-	@SuppressWarnings("unchecked")
-	User save(User user);
+    Optional<User> findByUsername(String username);
 
 }
