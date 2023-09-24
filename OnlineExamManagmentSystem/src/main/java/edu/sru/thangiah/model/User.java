f22f633ef92246495ac3408cd286d9f666829cc2
Seth Daniel Chritzman <142919196+sdc1004@users.noreply.github.com>
2023-09-24 16:33:35 -0400

    Removed the cascading type

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/model/User.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/model/User.java
@@ -55,7 +55,7 @@ public class User {
     
     private boolean enabled;
 
-	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles", 
                joinColumns = @JoinColumn(name = "user_id"), 
                inverseJoinColumns = @JoinColumn(name = "role_id"))
