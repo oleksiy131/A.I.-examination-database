f99255678923966089562038252cda74ccc41217
Seth Daniel Chritzman <142919196+sdc1004@users.noreply.github.com>
2023-09-24 16:10:44 -0400

    Creates a dynamic user at system startup.

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/OnlineExamManagmentSystemApplication.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/OnlineExamManagmentSystemApplication.java
@@ -16,17 +16,73 @@
 package edu.sru.thangiah;
 
 
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import edu.sru.thangiah.model.Roles;
+import edu.sru.thangiah.model.User;
+import edu.sru.thangiah.repository.RoleRepository;
+import edu.sru.thangiah.repository.UserRepository;
 
 @SpringBootApplication
 public class OnlineExamManagmentSystemApplication {
+    
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineExamManagmentSystemApplication.class, args);
-		
-		
 	}
+
+	@Bean
+	public CommandLineRunner setupRoles(RoleRepository roleRepository) {
+		
+
+	    return args -> {
+	        // Check and insert roles if they don't exist
+	        if (!roleRepository.findByName("ADMINISTRATOR").isPresent()) {
+	            roleRepository.save(new Roles(null, "ADMINISTRATOR"));
+	        }
+	        if (!roleRepository.findByName("STUDENT").isPresent()) {
+	            roleRepository.save(new Roles(null, "STUDENT"));
+	        }
+	        if (!roleRepository.findByName("INSTRUCTOR").isPresent()) {
+	            roleRepository.save(new Roles(null, "INSTRUCTOR"));
+	        }
+	        if (!roleRepository.findByName("SCHEDULE_MANAGER").isPresent()) {
+	            roleRepository.save(new Roles(null, "SCHEDULE_MANAGER"));
+	        }
+	    };
+	}
+	    @Bean
+	    public CommandLineRunner setupDefaultUser(UserRepository userRepository, RoleRepository roleRepository) {
+	        return args -> {
+	            User root = new User(null, null, null, null, null, null, null, false, null);
+	            root.setUsername("root");
+	            
+				root.setPassword(passwordEncoder.encode("software"));
+	            
+	            Roles adminRole = roleRepository.findByName("ADMINISTRATOR")
+	                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+	            root.setRoles(Collections.singleton(adminRole));
+	            
+	            if (!userRepository.findByUsername(root.getUsername()).isPresent()) {
+	                userRepository.save(root);
+	            }
+	        };
+	   
+
+	}
+	
+	
+
 }
 
 
