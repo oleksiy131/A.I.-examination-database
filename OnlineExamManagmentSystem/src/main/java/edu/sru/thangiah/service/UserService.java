b3c5b396cc2ae02bdef207cd1dda51465a93a31a
Seth Daniel Chritzman <142919196+sdc1004@users.noreply.github.com>
2023-09-25 14:20:55 -0400

    Incorperated a way to find the user by name

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/service/UserService.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/service/UserService.java
@@ -1,9 +1,17 @@
 package edu.sru.thangiah.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
 import edu.sru.thangiah.model.User;
 import edu.sru.thangiah.web.dto.UserRegistrationDto;
 
-public interface UserService {
+@Service
+public interface UserService extends UserDetailsService{
 	User save(UserRegistrationDto registrationDto);
+	UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
 
 }
