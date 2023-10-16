bf6d1bed3eff33bcaaad6316b9c3bda84fd1bf48
Oleksii Dukhovenko <oxd1005@sru.edu>
2023-10-16 10:37:05 -0400

    MultipleChoice[2.5]

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/controller/ExamController.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/controller/ExamController.java
@@ -3,21 +3,21 @@ package edu.sru.thangiah.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import edu.sru.thangiah.domain.Question;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import edu.sru.thangiah.service.ExamService;
-
+import edu.sru.thangiah.domain.Question;
 import java.util.List;
 
 @Controller
 public class ExamController {
+
     @Autowired
     private ExamService examService;
 
-    @GetMapping("/generateExam")
-    public String generateExam(Model model) {
-        List<Question> questions = examService.getRandomQuestions();
+    @RequestMapping("/generateExam/{chapter}")
+    public String generateExam(Model model, @PathVariable("chapter") int chapter) {
+        List<Question> questions = examService.generateExam(chapter, 10);
         model.addAttribute("questions", questions);
         return "exam";
     }
--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/service/ExamService.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/service/ExamService.java
@@ -3,38 +3,22 @@ package edu.sru.thangiah.service;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
-
 import edu.sru.thangiah.domain.Question;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
-
-/*
- *____  __    __        _ _ 
- / __ \/ /__ / /__ ___ (_|_)
-/ /_/ / / -_)  '_/(_-</ / / 
-\____/_/\__/_/\_\/___/_/_/  
-                        
- */
-
-
+import java.util.stream.Collectors;
 
 @Service
 public class ExamService {
-	
-    // Constants for file paths
-    private static final String MULTIPLE_CHOICE_FILE_PATH = "/static/chapter-4.xlsx";
-    private static final String TRUE_FALSE_FILE_PATH = "/static/chapterOneTF.xlsx";
 
-    // List to hold all questions
     private List<Question> allQuestions = new ArrayList<>();
 
-    // Constructor to read all questions into memory
     public ExamService() {
         readAllQuestions();
     }
 
-    // Method to get a random set of questions
     public List<Question> getRandomQuestions() {
         List<Question> selectedQuestions = new ArrayList<>();
         Collections.shuffle(allQuestions);
@@ -43,9 +27,8 @@ public class ExamService {
         return selectedQuestions;
     }
 
- // Method to read multiple-choice questions from Excel
     private void readMultipleChoiceQuestions() {
-        try (InputStream is = getClass().getResourceAsStream(MULTIPLE_CHOICE_FILE_PATH);
+        try (InputStream is = getClass().getResourceAsStream("/static/chapter-4.xlsx");
              Workbook workbook = new XSSFWorkbook(is)) {
             Sheet sheet = workbook.getSheetAt(0);
             for (int i = 0; i < sheet.getPhysicalNumberOfRows(); ) {
@@ -56,7 +39,6 @@ public class ExamService {
                 Question question = new Question();
                 question.setQuestionText(getCellValue(questionRow.getCell(0)));
                 Map<String, String> options = new HashMap<>();
-                // Assuming there are always 4 options
                 for (int j = 1; j <= 4; j++) {
                     Row optionRow = sheet.getRow(i + j);
                     if (optionRow == null || optionRow.getCell(0) == null || optionRow.getCell(1) == null) {
@@ -65,30 +47,24 @@ public class ExamService {
                     options.put(getCellValue(optionRow.getCell(0)), getCellValue(optionRow.getCell(1)));
                 }
                 question.setOptions(options);
-
-                // New code to read the answer, which is located after the options
-                i += 5; // Move to the answer row
+                i += 5;
                 Row answerRow = sheet.getRow(i);
                 if (answerRow != null && answerRow.getCell(0) != null) {
                     String answer = getCellValue(answerRow.getCell(0));
-                    // Extract the actual answer from the "Ans: " prefix
                     if (answer.startsWith("Ans: ")) {
-                        question.setCorrectAnswer(answer.substring(5).trim()); // Set the answer, excluding the "Ans: " part
+                        question.setCorrectAnswer(answer.substring(5).trim());
                     }
                 }
-
                 allQuestions.add(question);
-                i++; // Move to the next question block
+                i++;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    // Method to read true/false questions from Excel
     private void readTrueFalseQuestions() {
-        try (InputStream is = getClass().getResourceAsStream(TRUE_FALSE_FILE_PATH);
+        try (InputStream is = getClass().getResourceAsStream("/static/chapterOneTF.xlsx");
              Workbook workbook = new XSSFWorkbook(is)) {
             Sheet sheet = workbook.getSheetAt(0);
             for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
@@ -109,13 +85,11 @@ public class ExamService {
         }
     }
 
-    // Method to read all questions (both multiple-choice and true/false)
     private void readAllQuestions() {
         readMultipleChoiceQuestions();
         readTrueFalseQuestions();
     }
 
-    // Utility method to get cell value as a string
     private String getCellValue(Cell cell) {
         if (cell.getCellType() == CellType.STRING) {
             return cell.getStringCellValue();
@@ -123,5 +97,86 @@ public class ExamService {
             return String.valueOf((int) cell.getNumericCellValue());
         }
         return "";
+    }
+
+    public List<Question> generateExam(int chapterOrExamType, int numberOfQuestions) {
+        List<Question> examQuestions = new ArrayList<>();
+
+        // Function to read questions from specific files and add them to the examQuestions list
+        java.util.function.Consumer<String> readQuestions = (String filePath) -> {
+            try (InputStream is = getClass().getResourceAsStream(filePath)) {
+                if (is != null) {
+                    examQuestions.addAll(readQuestionsFromExcel(is));
+                }
+            } catch (IOException e) {
+                e.printStackTrace(); // exception handling
+            }
+        };
+
+        if (chapterOrExamType <= 4) {
+            // For chapter exams, read both multiple-choice and true/false questions
+            readQuestions.accept("/static/chapter-" + chapterOrExamType + ".xlsx");
+            readQuestions.accept("/static/chapter-" + chapterOrExamType + "-tf.xlsx");
+        } else if (chapterOrExamType == 5) { // Mid-Term
+            // For the mid-term
+            readQuestions.accept("/static/chapter-1.xlsx");
+            readQuestions.accept("/static/chapter-1-tf.xlsx");
+            readQuestions.accept("/static/chapter-2.xlsx");
+            readQuestions.accept("/static/chapter-2-tf.xlsx");
+            // Add more chapters if needed
+        } else if (chapterOrExamType == 6) { // Final Exam
+            // For the final exam, aggregate questions from all chapters
+        	
+            for (int i = 1; i <= 4; i++) { // ASSUMING 4 CHAPTERS!!
+                readQuestions.accept("/static/chapter-" + i + ".xlsx");
+                readQuestions.accept("/static/chapter-" + i + "-tf.xlsx");
+            }
+        }
+
+        // Shuffle the collected questions
+        Collections.shuffle(examQuestions);
+
+        // Limit the number of questions based on the specified count
+        return examQuestions.stream()
+                            .limit(numberOfQuestions)
+                            .collect(Collectors.toList());
+    }
+
+
+    private List<Question> readQuestionsFromExcel(InputStream is) {
+        List<Question> questions = new ArrayList<>();
+        try (Workbook workbook = new XSSFWorkbook(is)) {
+            Sheet sheet = workbook.getSheetAt(0);
+            for (int i = 0; i < sheet.getPhysicalNumberOfRows(); ) {
+                Row questionRow = sheet.getRow(i);
+                if (questionRow == null || questionRow.getCell(0) == null) {
+                    continue;
+                }
+                Question question = new Question();
+                question.setQuestionText(getCellValue(questionRow.getCell(0)));
+                Map<String, String> options = new HashMap<>();
+                for (int j = 1; j <= 4; j++) {
+                    Row optionRow = sheet.getRow(i + j);
+                    if (optionRow == null || optionRow.getCell(0) == null || optionRow.getCell(1) == null) {
+                        continue;
+                    }
+                    options.put(getCellValue(optionRow.getCell(0)), getCellValue(optionRow.getCell(1)));
+                }
+                question.setOptions(options);
+                i += 5;
+                Row answerRow = sheet.getRow(i);
+                if (answerRow != null && answerRow.getCell(0) != null) {
+                    String answer = getCellValue(answerRow.getCell(0));
+                    if (answer.startsWith("Ans: ")) {
+                        question.setCorrectAnswer(answer.substring(5).trim());
+                    }
+                }
+                questions.add(question);
+                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return questions;
     }
 }
