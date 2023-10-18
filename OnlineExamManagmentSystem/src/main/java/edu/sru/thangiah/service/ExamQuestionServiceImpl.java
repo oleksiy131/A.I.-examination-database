98f5fd25fcc778e660a4d6cb0327dc0fc9ad4378
Oleksii Dukhovenko <oxd1005@sru.edu>
2023-10-18 09:24:32 -0400

    GroundWorkLaid

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/service/ExamQuestionServiceImpl.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/service/ExamQuestionServiceImpl.java
@@ -10,6 +10,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class ExamQuestionServiceImpl implements ExamQuestionService {
@@ -22,27 +24,36 @@ public class ExamQuestionServiceImpl implements ExamQuestionService {
         // Define the path to your text file
         String filePath = "/static/chapter-1.txt";
 
+        // Regular expression to identify the question number and text
+        Pattern questionPattern = Pattern.compile("^(\\d+)\\.(.*)");
+
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             ExamQuestion currentExamQuestion = null;
+            boolean isQuestion = false;
 
             while ((line = reader.readLine()) != null) {
-                if (line.trim().isEmpty()) {
-                    // Skip empty lines
-                    continue;
-                }
+                line = line.trim();
 
-                // Check if the line starts with a number (question number)
-                if (Character.isDigit(line.charAt(0))) {
+                if (line.isEmpty()) {
+                    // Empty line indicates the end of the current question
                     if (currentExamQuestion != null) {
                         // Save the previous exam question
                         examQuestionRepository.save(currentExamQuestion);
+                        currentExamQuestion = null;
+                        isQuestion = false;
                     }
+                    continue;
+                }
+
+                Matcher matcher = questionPattern.matcher(line);
+                if (matcher.matches()) {
+                    // This line is the start of a new question
+                    isQuestion = true;
                     currentExamQuestion = new ExamQuestion();
-                    String[] parts = line.split("\\.", 2);
-                    currentExamQuestion.setQuestionText(parts[1].trim());
-                } else if (currentExamQuestion != null) {
-                    // Parse answer options and correct answer
+                    currentExamQuestion.setQuestionText(matcher.group(2).trim()); // Group 2 is the question text
+                } else if (isQuestion) {
+                    // We are within a question - parse answer options and correct answer
                     if (line.startsWith("A.")) {
                         currentExamQuestion.setOptionA(line.substring(2).trim());
                     } else if (line.startsWith("B.")) {
@@ -57,7 +68,7 @@ public class ExamQuestionServiceImpl implements ExamQuestionService {
                 }
             }
 
-            // Save the last exam question after reading the file
+            // If the file didn't end with an empty line, we need to save the last question
             if (currentExamQuestion != null) {
                 examQuestionRepository.save(currentExamQuestion);
             }
