302585b2082613d758fc9b2d9443800192139eb3
Seth Daniel Chritzman <142919196+sdc1004@users.noreply.github.com>
2023-11-12 13:32:22 -0500

    Added method to find exam by course id.

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/repository/ExamRepository.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/repository/ExamRepository.java
@@ -1,8 +1,13 @@
 package edu.sru.thangiah.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import edu.sru.thangiah.domain.Course;
 import edu.sru.thangiah.domain.Exam;
 
 public interface ExamRepository extends JpaRepository<Exam, Long> {
-}
+
+	Set<Exam> findByCourse(Course course);
+	}
