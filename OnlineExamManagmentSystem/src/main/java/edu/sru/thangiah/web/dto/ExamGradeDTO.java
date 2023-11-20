03bb7fa4978cffcdd0570f66c8ec3cb85b9a1961
Seth Daniel Chritzman <142919196+sdc1004@users.noreply.github.com>
2023-11-20 17:18:47 -0500

    Methods to support Exam Grade % and score/totalQuestions

--- OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/web/dto/ExamGradeDTO.java
+++ OnlineExamManagmentSystem/src/main/java/edu/sru/thangiah/web/dto/ExamGradeDTO.java
@@ -3,10 +3,12 @@ package edu.sru.thangiah.web.dto;
 public class ExamGradeDTO {
     private String examName;
     private Integer score; // Use Integer to handle null values if there's no submission
+    private Integer totalQuestions; // Total number of questions for the exam
 
-    public ExamGradeDTO(String examName, Integer score) {
+    public ExamGradeDTO(String examName, Integer score, Integer totalQuestions) {
         this.examName = examName;
         this.score = score;
+        this.totalQuestions = totalQuestions;
     }
 
 	public String getExamName() {
@@ -24,6 +26,48 @@ public class ExamGradeDTO {
 	public void setScore(Integer score) {
 		this.score = score;
 	}
+	
+	public Integer getTotalQuestions() {
+        return totalQuestions;
+    }
+
+    public void setTotalQuestions(Integer totalQuestions) {
+        this.totalQuestions = totalQuestions;
+    }
+
+    public String getFormattedScore() {
+        if (score == null || totalQuestions == null) {
+            return "Not available";
+        }
+        return String.format("%d/%d", score, totalQuestions);
+    }
+    
+ // Method to calculate the letter grade
+    public String getLetterGrade() {
+        if (score == null || totalQuestions == null || totalQuestions == 0) {
+            return "N/A"; // Not Available or Not Applicable
+        }
+        double percentage = (double) score / totalQuestions * 100;
+        if (percentage >= 90) {
+            return "A";
+        } else if (percentage >= 80) {
+            return "B";
+        } else if (percentage >= 70) {
+            return "C";
+        } else if (percentage >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+    
+    public String getPercentage() {
+        if (score == null || totalQuestions == null || totalQuestions == 0) {
+            return "Not available";
+        }
+        int percentage = (int) ((double) score / totalQuestions * 100);
+        return percentage + "%";
+    }
 
     
 }
